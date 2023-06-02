d70674001b907204ca1fa8f41f3c8c0ec9f30028
Sergey Bendak <sleepoke@gmail.com>
2023-06-02 16:43:36 +0300

    create custom admin for models from recipes app

--- backend/src/foodgram/settings.py
+++ backend/src/foodgram/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-clrt3e_5eynrrg5r!i1tkz5&*5jb$#lbb&)mk!8p^l_nbnoig7'
+SECRET_KEY = 'django-insecure-clrt3e_5eynrrg5r!i1tkz5&*5jb$#lbb&)mk!8p^l_nbnoig7' # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes',
 ]
 
 MIDDLEWARE = [
@@ -86,16 +87,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', # noqa
     },
 ]
 
--- backend/src/recipes/admin.py
+++ backend/src/recipes/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Recipe, Tag, Ingredient, RecipeIngredient, RecipeTag, User
 
-# Register your models here.
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'get_favorites_count')
+    list_filter = ('author', 'title', 'tags')
+
+    def get_favorites_count(self, obj):
+        return obj.favorites.count()
+
+    get_favorites_count.short_description = 'Number of favorites'
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    list_filter = ('name',)
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('email', 'username')
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Tag)
+admin.site.register(RecipeIngredient)
+admin.site.register(RecipeTag)
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
