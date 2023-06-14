2451b4f31d0530e51ed3aba3ad955db5e1793c52
Sergey Bendak <sleepoke@gmail.com>
2023-06-14 22:55:59 +0300

    update

--- backend/src/foodgram/settings.py
+++ backend/src/foodgram/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
     "default": {
         'ENGINE': os.getenv('DB_ENGINE',
                             default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='foodgram'),
+        'NAME': os.getenv('POSTGRES_DB', default='foodgram'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
         'HOST': os.getenv('DB_HOST', default='db'),
