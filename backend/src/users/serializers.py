697ad611af91b531552c821514e587adecbc98a0
Sergey Bendak <sleepoke@gmail.com>
2023-06-16 12:24:07 +0300

    fix subscriptions

--- backend/src/users/serializers.py
+++ backend/src/users/serializers.py
@@ -2,16 +2,26 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from recipes.models import Recipe
+from .models import Subscription
 
 User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = ('email', 'id', 'username', 'first_name', 'last_name',
                   'is_subscribed', 'password')
         extra_kwargs = {'password': {'write_only': True}}
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request', None)
+        if request:
+            return Subscription.objects.filter(
+                user=request.user, author=obj).exists()
+        return False
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
@@ -37,7 +47,7 @@ class SetPasswordSerializer(serializers.Serializer):
 
 
 class UserSubscriptionSerializer(serializers.ModelSerializer):
-    is_subscribed = serializers.BooleanField(source='user.is_subscribed')
+    is_subscribed = serializers.SerializerMethodField()
     recipes = serializers.SerializerMethodField()
     recipes_count = serializers.SerializerMethodField()
 
@@ -53,6 +63,13 @@ class UserSubscriptionSerializer(serializers.ModelSerializer):
             author=obj
         )[:self.context['request'].query_params.get('recipes_limit', 5)]
         return ShortRecipeSerializer(recipes, many=True).data
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request', None)
+        if request:
+            return Subscription.objects.filter(
+                user=request.user, author=obj).exists()
+        return False
 
     def get_recipes_count(self, obj):
         return Recipe.objects.filter(author=obj).count()
