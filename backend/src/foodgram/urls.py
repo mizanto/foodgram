a509abe37b29edb047e5dd99848ae3a4c45c691f
Sergey Bendak <sleepoke@gmail.com>
2023-06-18 11:28:51 +0300

    fix urls

--- backend/src/foodgram/urls.py
+++ backend/src/foodgram/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/recipes/', include('recipes.urls')),
-    path('api/users/', include('users.urls')),
+    path('api/', include('recipes.urls')),
+    path('api/', include('users.urls')),
 ]
