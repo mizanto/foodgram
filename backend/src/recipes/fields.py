8aaa79111b24717ad4a04fda61c84d6c44d2f0b4
Sergey Bendak <sleepoke@gmail.com>
2023-06-22 11:40:57 +0300

    fix image extension parsing

--- backend/src/recipes/fields.py
+++ backend/src/recipes/fields.py
@@ -1,5 +1,6 @@
 import base64
 import imghdr
+import mimetypes
 import uuid
 
 from django.core.files.base import ContentFile
@@ -8,15 +9,22 @@ from rest_framework import serializers
 
 class Base64ImageField(serializers.ImageField):
     def to_internal_value(self, data):
-        if isinstance(data, str):
-            if 'data:' in data and ';base64,' in data:
-                _, data = data.split(';base64,')
+        if isinstance(data, str) and 'data:' in data and ';base64,' in data:
+            header, data = data.split(';base64,')
+            mime_type = header.split(':')[-1]
+            extension = mimetypes.guess_extension(mime_type)
+
+            if extension not in ['.jpg', '.jpeg', '.png', '.bmp']:
+                raise serializers.ValidationError('Invalid image type.')
+
             try:
                 decoded_file = base64.b64decode(data)
             except TypeError:
                 self.fail('invalid_image')
-            data = ContentFile(
-                decoded_file, name=self.get_file_name(decoded_file))
+
+            file_name = self.get_file_name(decoded_file)
+            data = ContentFile(decoded_file, name=f'{file_name}{extension}')
+
         return super().to_internal_value(data)
 
     def get_file_name(self, decoded_file, length=12):
